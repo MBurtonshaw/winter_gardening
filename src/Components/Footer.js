61ef625fafdd0e4984108df965f55fd5e72a1a66
Matt Burtonshaw <56322461+MBurtonshaw@users.noreply.github.com>
2024-08-04 14:21:16 -0400

    Add files via upload

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,8 +2,8 @@ import { React } from 'react';
 
 export default function Footer() {
       return(
-        <div id="footer" className="row py-2">
-            <small>By: Matthew Burtonshaw</small>
+        <div id="footer" className="py-3 w-100 m-auto">
+            <a className='nonchalant' target="_blank" href='https://verdant-cranachan-f205b0.netlify.app/'>by Matthew Burtonshaw</a>
         </div>
       );
 }
